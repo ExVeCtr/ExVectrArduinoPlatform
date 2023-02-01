13e2f976112b965647e3338e0562fee89fdec0df
ChSt15 <chrissteffen98@gmail.com>
2023-02-01 21:55:30 +0100

    Removed unneeded stuff

--- include/ExVectrArduinoPlatform/time_implementation.hpp
+++ include/ExVectrArduinoPlatform/time_implementation.hpp
@@ -6,29 +6,7 @@
 namespace VCTR
 {
 
-    /**
-     * @brief Remember to add documentation to all functions. This one returns the current system time in seconds.
-     * @note Will overflow after ~292.47 years. If a deployed system runs longer then that and crashes, then give my great great grandchildren a call.
-     *  
-     * @returns time since system start in seconds.
-     */
-    extern float yourSpecialFunctionDefined();
-
-    /**
-     * A super duper class than has no actual use other than containing a super duper function to test.
-    */
-    class SomeSuperDuperClass {
-        public:
-
-        /**
-         * @brief A super duper function to greet the user.
-         * @param systemTime The current system time in seconds since start.
-         * 
-         * @returns a cool string to greet the user.
-        */
-        const char* aSuperDuperFunction(float systemTime);
-
-    };
+    
 
 }
 
